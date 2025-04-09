27355cd923873b9f8f95860958305a2a687a043d
Paul Sproxton <sproxton.paul@gmail.com>
2025-04-09 17:16:17 -0400

    Start the UI and Redux components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import './App.css'
-import { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import Alert from './components/layout/Alert'
+// Redux
+import { Provider } from 'react-redux'
+import store from './store'
 
-const App = () => (
-	<Fragment>
-		<Navbar />
-		<Landing />
-	</Fragment>
-)
+const App = () => {
+	return (
+		<Provider store={store}>
+			<Router>
+				<Navbar />
+				<Alert />
+				<Routes>
+					<Route path='/' element={<Landing />} />
+					<Route path='register' element={<Register />} />
+					<Route path='login' element={<Login />} />
+				</Routes>
+			</Router>
+		</Provider>
+	)
+}
 
 export default App
